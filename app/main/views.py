474311cebc97f49368485a27a4e4ef16f6ca9b85
mzazakeith <mzazakeith@gmail.com>
2018-06-28 10:23:36 +0300

    all importations

--- app/main/views.py
+++ app/main/views.py
@@ -3,6 +3,12 @@ from sqlalchemy import func
 
 from . import main
 from ..models import User, Promotion, Pick, Production, Interview, CommentsPromotion, CommentsPick, CommentsProduction, \
+    CommentsInterview, Like, Unlike
+from .forms import UpdateProfile, PromotionForm, PickForm, ProductionForm, InterviewForm, PromotionCommentForm, \
+    PickCommentForm, ProductionCommentForm, InterviewCommentForm
+from flask_login import login_required, current_user
+
+from .. import db, photos
 @main.route('/')
 def index():
     """
