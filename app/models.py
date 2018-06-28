fa6c4c5e2fdc704a90367e7323e69d3a2328f866
mzazakeith <mzazakeith@gmail.com>
2018-06-28 10:07:54 +0300

    User class added

---
+++ app/models.py
@@ -0,0 +1,9 @@
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), unique=True)
+    email = db.Column(db.String(255), unique=True, index=True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
